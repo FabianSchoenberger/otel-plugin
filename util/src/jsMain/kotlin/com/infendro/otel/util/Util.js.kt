fbce00e60c7407a6fc838a30f9bd960235c0cabb
FabianSchoenberger <163822878+FabianSchoenberger@users.noreply.github.com>
2025-05-30 11:30:17 +0200

    add await with debug print

--- util/src/jsMain/kotlin/com/infendro/otel/util/Util.js.kt
+++ util/src/jsMain/kotlin/com/infendro/otel/util/Util.js.kt
@@ -4,6 +4,8 @@ import com.infendro.otlp.OtlpExporter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 import node.process.process
 
 actual fun await(
@@ -11,6 +13,21 @@ actual fun await(
 ) {
     CoroutineScope(Dispatchers.Default).launch {
         exporter.await()
+        process.exit()
+    }
+}
+
+actual fun await(
+    exporter: OtlpExporter,
+    start: Instant
+) {
+    CoroutineScope(Dispatchers.Default).launch {
+        exporter.await()
+
+        val end = Clock.System.now()
+        val ms = (end - start).inWholeMilliseconds
+        println("Flush finished - $ms ms elapsed")
+
         process.exit()
     }
 }
